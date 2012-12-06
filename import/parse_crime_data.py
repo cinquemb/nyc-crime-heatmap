7a416b2c0b67d1fdf557e58f1f886459269be167
Cinque McFarlane-Blake <cinque_mcfarlane-blake@brown.edu>
2012-12-05 19:04:59 -0500

    changing some variable names

--- import/parse_crime_data.py
+++ import/parse_crime_data.py
@@ -4,9 +4,9 @@ import time
 class Command(BaseCommand):
     help = 'Parses json from nyc database and outputs selected data to be fed into map'
     def handle(self, *args, **options):
-        latlngString = requests.get("http://data.cityofnewyork.us/api/views/maj4-ux6k/rows.json")
+        crimes = requests.get("http://data.cityofnewyork.us/api/views/maj4-ux6k/rows.json")
         #time.sleep(.3)
-        crimes = json.loads(latlngString.text)
+        crimedata = json.loads(crimes.text)
             
-        data = crimes['data']
-        format = crimes['meta']['format']
+        data = crimedata['data']
+        format = crimedata['meta']['format']
