7ee2528b1b3873988cc73e38707942a5a2446873
cinquemb <cinque_mcfarlane-blake@brown.edu>
2013-05-10 23:13:43 -0400

    maor ish

--- heatmap/import/management/commands/parse_crime_data.py
+++ heatmap/import/management/commands/parse_crime_data.py
@@ -1,19 +1,32 @@
 import requests
 import json
 import time
+import os
 from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
     help = 'Parses json from nyc database and outputs selected data to be fed into map'
+
     def handle(self, *args, **options):
     	#All 311 Service Requests from 2010 to present. This information is automatically updated daily
-        crimes = requests.get("http://data.cityofnewyork.us/api/views/maj4-ux6k/rows.json")
+        crimes = requests.get("http://data.cityofnewyork.us/api/views/maj4-ux6k/rows.json?max_rows=10")
         #time.sleep(.3)
-        crimedata = json.loads(crimes.text)
+        raw_static_path = os.path.join(os.path.abspath(os.path.dirname('import/management/commands/')), 'raw.json')
+    	raw_static_file = open(raw_static_path, "w")
+        #crimedata = json.loads()
+        raw_static_file.write(crimes.text)
+
+        raw_static_file.close()
+
         #All Columns
-        format = crimedata['meta']['view']['columns']
+        #format = crimedata['meta']['view']['columns']
         #Fields in each Column (loop through and get it): "id","name","dataTypeName","fieldName","position","renderTypeName","format"
+        
         #Data for Columns (loop through each array to access comma seperated array with each position corresponding to column values)
-        data = crimedata['data']
+        #data = crimedata['data']
+
+        print 'Done'
+
+        
         
